e4571b39e8ed9fb83d0f69f1669556e99b6c657e
O-l-g-a-B <bozhedomova_o@mail.ru>
2022-05-01 15:47:52 +0300

    static

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-  var op; 
+let op;
 
-  function func() {
-     var result;
-     var num1 = Number(document.getElementById("num1").value);
-   var num2 = Number(document.getElementById("num2").value);
-      switch (op) {
-     case '+':
-       result = num1 + num2;
-       break;
-     case '-':
-       result = num1 - num2;
-       break;
-     case '*':
-       result = num1 * num2;
-       break;
-     case '/':
-       result = num2 ? num1 / num2 : "На ноль делить нельзя!";
-       break;
-   }
-   
-document.getElementById("result").innerHTML = result;
- }
- 
+function func() {
+  let result;
+  let num1 = Number(document.getElementById("num1").value);
+  let num2 = Number(document.getElementById("num2").value);
+  class Calculator {
+    static addition() {
+      result = num1 + num2;
+    }
+    static subtraction() {
+      result = num1 - num2;
+    }
+    static multiplication() {
+      result = num1 * num2;
+    }
+    static division() {
+      result = num2 ? num1 / num2 : "На ноль делить нельзя!";
+    }
 
+  }
+  switch (op) {
+    case '+':
+      Calculator.addition();
+      break;
+    case '-':
+      Calculator.subtraction();
+      break;
+    case '*':
+      Calculator.multiplication();
+      break;
+    case '/':
+      Calculator.division();
+      break;
+  }
+  document.getElementById("result").innerHTML = result;
+}
